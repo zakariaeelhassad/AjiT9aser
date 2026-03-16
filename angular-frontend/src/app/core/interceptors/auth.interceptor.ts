31eb73961626efc113540663b56542d39ef42700
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-16 11:24:19 +0000

    créer l'intercepteur Angular pour injecter le jeton JWT

---
+++ angular-frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    constructor() { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (typeof window !== 'undefined' && window.localStorage) {
+            const token = localStorage.getItem('token');
+            if (token) {
+                const authReq = request.clone({
+                    headers: request.headers.set('Authorization', `Bearer ${token}`)
+                });
+                return next.handle(authReq);
+            }
+        }
+        return next.handle(request);
+    }
+}
