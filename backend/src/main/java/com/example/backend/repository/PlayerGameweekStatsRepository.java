bc9b68c3a197a76a4b2e936d830e120302401244
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-24 23:10:33 +0100

    Mise à jour du repository PlayerGameweekStats

--- backend/src/main/java/com/example/backend/repository/PlayerGameweekStatsRepository.java
+++ backend/src/main/java/com/example/backend/repository/PlayerGameweekStatsRepository.java
@@ -1,6 +1,6 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.PlayerGameweekStats;
+import com.example.backend.model.entity.PlayerGameweekStats;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
