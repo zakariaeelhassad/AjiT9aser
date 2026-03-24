ad23220e1959c8201a4fb9dd39b72fbba7285e5f
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-24 23:10:34 +0100

    Mise à jour du repository UserTeamGameweekLineup

--- backend/src/main/java/com/example/backend/repository/UserTeamGameweekLineupRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserTeamGameweekLineupRepository.java
@@ -1,6 +1,6 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.UserTeamGameweekLineup;
+import com.example.backend.model.entity.UserTeamGameweekLineup;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
