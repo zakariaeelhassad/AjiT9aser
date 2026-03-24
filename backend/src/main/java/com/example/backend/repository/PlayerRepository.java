8e5d34587f6b9e34ec84c4738083d8990e4d72d6
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-24 23:10:33 +0100

    Mise à jour du repository Player

--- backend/src/main/java/com/example/backend/repository/PlayerRepository.java
+++ backend/src/main/java/com/example/backend/repository/PlayerRepository.java
@@ -1,7 +1,7 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.Player;
-import com.example.backend.model.Position;
+import com.example.backend.model.entity.Player;
+import com.example.backend.model.enums.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
