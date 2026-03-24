ca708169c9e6e24a87aac2654a4b6942b4182006
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-24 23:10:35 +0100

    Mise à jour du repository UserTeamGameweekTransfers

--- backend/src/main/java/com/example/backend/repository/UserTeamGameweekTransfersRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserTeamGameweekTransfersRepository.java
@@ -1,6 +1,6 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.UserTeamGameweekTransfers;
+import com.example.backend.model.entity.UserTeamGameweekTransfers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
