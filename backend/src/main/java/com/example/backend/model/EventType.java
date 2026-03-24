f012a71bfdc38faf11ab48d3b3e46a82ddfd465c
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2026-03-24 23:21:51 +0100

    Supprimer les anciennes classes de modèles

--- backend/src/main/java/com/example/backend/model/EventType.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.model;
-
-public enum EventType {
-    GOAL,
-    ASSIST,
-    YELLOW_CARD,
-    RED_CARD,
-    APPEARANCE
-}
--- backend/src/main/java/com/example/backend/model/Position.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.model;
-
-public enum Position {
-    GK, // Goalkeeper
-    DEF, // Defender
-    MID, // Midfielder
-    FWD // Forward
-}
